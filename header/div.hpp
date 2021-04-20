01e1eab3b8df401cd15ea8216a298d45ab3a821e
fadi001 <fzaki001@ucr.edu>
2021-04-20 13:33:57 -0700

    Division Func

---
+++ header/div.hpp
@@ -0,0 +1,33 @@
+
+#ifndef __DIV_HPP__
+#define __DIV_HPP__
+
+#include "base.hpp"
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+ using namespace std;
+
+ class Div : public Base {
+    public:
+        Div(Base* lhs, Base* rhs) : Base(), left(lhs), right(rhs) { }
+        virtual double evaluate() { 
+		double lhse = left->evaluate();
+		double rhse = right->evaluate();
+	return (rhse/lhse); }
+
+        virtual string stringify() { 
+		string rhss = left->stringify();
+		string lhss = right->stringify();
+
+	ostringstream ss;
+        ss << setprecision(8) << noshowpoint << '(' << lhss << '/' << noshowpoint << rhss << ')';
+	return  ss.str(); }
+
+    private: 
+	Base* left;
+	Base* right;
+};
+
+#endif
