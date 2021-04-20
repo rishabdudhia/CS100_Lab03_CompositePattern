98bbf68c8938e8277f0eb2296bb47ad1eabf4e8c
fadi001 <fzaki001@ucr.edu>
2021-04-20 12:47:28 -0700

    Added tests and and tested Mult

--- mocks/mock_funcs.hpp
+++ mocks/mock_funcs.hpp
@@ -1,7 +1,7 @@
 #ifndef __MOCK_FUNCS_HPP__
 #define __MOCK_FUNCS_HPP__
 
-#include "base.hpp"
+#include "../header/base.hpp"
 #include "iostream"
 #include <sstream>
 #include <iomanip>
@@ -12,7 +12,7 @@ class SevenOpMock: public Base {
 
         virtual double evaluate() { return 7.5; }
         virtual string stringify() { return "7.5"; }
-}
+};
 
 class NegFiveOpMock: public Base {
     public:
@@ -20,7 +20,15 @@ class NegFiveOpMock: public Base {
 
         virtual double evaluate() { return -5; }
         virtual string stringify() { return "-5"; }
-}
+};
+
+class fiveOpMock: public Base {
+    public:
+       fiveOpMock() { }
+
+        virtual double evaluate() { return 5; }
+        virtual string stringify() { return "5"; }
+};
 
 class ZeroOpMock: public Base {
     public:
@@ -28,7 +36,7 @@ class ZeroOpMock: public Base {
 
         virtual double evaluate() { return 0; }
         virtual string stringify() { return "0"; }
-}
+};
 
 class PosDivisionTwentyoneBySevenOpMock: public Base {
     public:
@@ -36,7 +44,7 @@ class PosDivisionTwentyoneBySevenOpMock: public Base {
 
         virtual double evaluate() { return (21/7); }
         virtual string stringify() { return "(21/7)"; }
-}
+};
 
 class NegDivisionTwentyoneByThreeOpMock: public Base {
     public:
@@ -44,7 +52,7 @@ class NegDivisionTwentyoneByThreeOpMock: public Base {
 
         virtual double evaluate() { return (-21/3); }
         virtual string stringify() { return "(-21/3)"; }
-}
+};
 
 class NegDivisionSevetyFiveByFifteenOpMock: public Base {
     public:
@@ -52,7 +60,7 @@ class NegDivisionSevetyFiveByFifteenOpMock: public Base {
 
         virtual double evaluate() { return (-75.5/15); }
         virtual string stringify() { return "(-75.5/15)"; }
-}
+};
 
 class PosADD15plus3: public Base {
     public:
@@ -60,54 +68,54 @@ class PosADD15plus3: public Base {
 
         virtual double evaluate() { return (18); }
         virtual string stringify() { return "(15+3)"; }
-
+};
 class PosADD12plus19: public Base {
     public:
        PosADD12plus19() { }
 
         virtual double evaluate() { return (31); }
         virtual string stringify() { return "(12+19)"; }
-
+};
 class NegplusPos: public Base {
     public:
        NegplusPos() { }
 
         virtual double evaluate() { return (12); }
         virtual string stringify() { return "(-3+15)"; }
-
+};
 class PosSubtraction: public Base {
     public:
        PosSubtraction() { }
 
         virtual double evaluate() { return (3); }
         virtual string stringify() { return "(10-7)"; }
-
+};
 class NegSubtraction: public Base {
     public:
        NegSubtraction() { }
 
         virtual double evaluate() { return (-3); }
         virtual string stringify() { return "(7-10)"; }
-
+};
 class Negpowthree: public Base {
     public:
        Negpowthree() { }
 
         virtual double evaluate() { return (-8); }
-        virtual string stringify() { return "(-2)**(3)"; }
-
+        virtual string stringify() { return "((-2)**(3))"; }
+};
 class Negpowtwo: public Base {
     public:
        Negpowtwo() { }
 
         virtual double evaluate() { return (4); }
-        virtual string stringify() { return "(-2)**(2)"; }
-
-class Pospowthree: public Base {
+        virtual string stringify() { return "((-2)**(2))"; }
+};
+class Pospowfour: public Base {
     public:
        Pospowfour() { }
 
         virtual double evaluate() { return (81); }
-        virtual string stringify() { return "(3)**(4)"; }
-
+        virtual string stringify() { return "((3)**(4))"; }
+};
 #endif
--- tests/mult_test.hpp
+++ tests/mult_test.hpp
@@ -2,9 +2,48 @@
 #define __ADD_TEST_HPP__
 
 #include "gtest/gtest.h"
-#include "../mock/mock_funcs.hpp"
+#include "../mocks/mock_funcs.hpp"
 #include "../header/mult.hpp"
 
+TEST(MultTest, MultTwoPositiveOperands) {
+    SevenOpMock* var1;
+    fiveOpMock* var2;
+    Mult* test = new Mult(var1, var2);
+    EXPECT_EQ(test->evaluate(), 37.5);
+    EXPECT_EQ(test->stringify(), "(7.5*5)");
+}
+
+TEST(MultTest, MultPositiveNegativeOperands) {
+    SevenOpMock* var1;
+    NegFiveOpMock* var2;
+    Mult* test = new Mult(var1, var2);
+    EXPECT_EQ(test->evaluate(), -37.5);
+    EXPECT_EQ(test->stringify(), "(7.5*-5)");
+}
+
+TEST(MultTest, MultOnePositiveOperandAndDivision) {
+    SevenOpMock* var1;
+   PosDivisionTwentyoneBySevenOpMock* var2;
+    Mult* test = new Mult(var1, var2);
+    EXPECT_EQ(test->evaluate(), 22.5);
+    EXPECT_EQ(test->stringify(), "(7.5*(21/7))");
+}
+
+TEST(MultTest, NegOpMultNegDivision) {
+    NegFiveOpMock* var1;
+    NegDivisionTwentyoneByThreeOpMock* var2;
+    Mult* test = new Mult(var1, var2);
+    EXPECT_EQ(test->evaluate(), 35);
+    EXPECT_EQ(test->stringify(), "(-5*(-21/3))");
+}
+
+TEST(MultTest, PowTimesNegSub) {
+    Negpowtwo* var1;
+    NegSubtraction* var2;
+    Mult* test = new Mult(var1, var2);
+    EXPECT_EQ(test->evaluate(), -12);
+    EXPECT_EQ(test->stringify(), "((-2)**(2))*(-21/3))");
+}
 
 
 #endif
